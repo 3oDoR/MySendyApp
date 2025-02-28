8c334afacb905d53f5461f41c21084b3b6e56001
Nikita <smirnov2.nv@gmail.com>
2025-02-28 17:22:33 +0300

    add lib and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,10 @@ android {
 
 dependencies {
 
+    implementation(files("libs/sendy_app_sdk.aar"))
+    implementation (libs.gson)
+    implementation (libs.nv.websocket.client)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +53,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
